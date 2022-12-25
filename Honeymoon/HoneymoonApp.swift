b5fd09bacc48242929ed5373d02f722688716846
Kakidiako Afonso <kakidiakoafonso.gmail.com>
2022-12-25 13:38:34 +0100

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Kakidiako Afonso on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
